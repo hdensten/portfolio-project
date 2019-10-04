2a147dfbcd7f6f677c9272e0697651b4e8678cbc
hdensten <hannah.densten@gmail.com>
2019-10-04 11:18:44 -0600

    More updates

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationComponent from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -13,16 +16,24 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
+            <h1>Hannah Densten's Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm a")}</div>
             <NavigationComponent />
+
             <Switch>
               <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about-me" component={About}></Route>
+              <Route path="/about-me" component={About}></Route>
+              <Route path="/contact" component={Contact}></Route>
+              <Route path="/blog" component={Blog}></Route>
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              ></Route>
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
-        <h1>Hannah Densten's Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm a")}</div>
-        <PortfolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,72 +1,92 @@
 import React, { Component } from "react";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to My Portfolio",
-            isLoading: false,
-            data: [
-                {title: "UMass Amherst", category: "Education"}, 
-                {title: "Tighe & Bond", category: "Civil Engineering"}, 
-                {title: "Bohler Engineering", category: "Civil Engineering"}, 
-                {title: "Bottega", category: "Education"}, 
-                {title: "Ivanti", category: "Software Development"}
-            ]
-        };
+    this.state = {
+      pageTitle: "Welcome to My Portfolio",
+      isLoading: false,
+      data: [
+        {
+          title: "UMass Amherst",
+          category: "Education",
+          slug: "umass-amherst"
+        },
+        {
+          title: "Tighe & Bond",
+          category: "Civil Engineering",
+          slug: "tighe-and-bond"
+        },
+        {
+          title: "Bohler Engineering",
+          category: "Civil Engineering",
+          slug: "bohler-engineering"
+        },
+        { title: "Bottega", category: "Education", slug: "bottega" },
+        { title: "Ivanti", category: "Software Development", slug: "ivanti" }
+      ]
+    };
 
-        this.handleFilter = this.handleFilter.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem title={item.title} url={"umass.edu"} slug={item.slug} />
+      );
+    });
+  }
+
+  handlePageTitleUpdate() {
+    this.setState({
+      pageTitle: "Something Else"
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
-    
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"umass.edu"}/>;
-        });
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    handlePageTitleUpdate() {
-        this.setState({
-            pageTitle: "Something Else" 
-        });
-    }
+        <div className="filter-buttons">
+          <button
+            onClick={() => this.handleFilter("Education")}
+            className="filter-button"
+          >
+            Education
+          </button>
+          <button
+            onClick={() => this.handleFilter("Civil Engineering")}
+            className="filter-button"
+          >
+            Civil Engineering
+          </button>
+          <button
+            onClick={() => this.handleFilter("Software Development")}
+            className="filter-button"
+          >
+            Software Development
+          </button>
+        </div>
 
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>;
-        }
-        
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <div className="filter-buttons">
-                    <button onClick={() => this.handleFilter("Education")} className="filter-button">
-                        Education
-                    </button>
-                    <button onClick={() => this.handleFilter("Civil Engineering")} className="filter-button">
-                        Civil Engineering
-                    </button>
-                    <button onClick={() => this.handleFilter("Software Development")} className="filter-button">
-                        Software Development
-                    </button>
-                </div>
-
-                <div className="portfolio-items">
-                    {this.portfolioItems()}
-                </div>
-                
-
-            </div>
-        )
-    }
+        <div className="portfolio-items">{this.portfolioItems()}</div>
+      </div>
+    );
+  }
 }
