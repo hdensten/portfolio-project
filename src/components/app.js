2cb540e97414d10065b6ca8adbaeeab7156babe7
hdensten <hannah.densten@gmail.com>
2019-10-04 09:22:43 -0600

    More updates

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 import PortfolioContainer from './portfolio/portfolio-container'; 
-import PortfolioItem from './portfolio/portfolio-item';
+import NavigationComponent from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationComponent />
         <h1>Hannah Densten's Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm a')}</div>
         <PortfolioContainer />
-        <PortfolioItem />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,17 +7,27 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to My Portfolio",
+            isLoading: false,
             data: [
-                {title: "UMass"}, 
-                {title: "Bohler"}, 
-                {title: "Bottega"}, 
-                {title: "Ivanti"}
+                {title: "UMass Amherst", category: "Education"}, 
+                {title: "Tighe & Bond", category: "Civil Engineering"}, 
+                {title: "Bohler Engineering", category: "Civil Engineering"}, 
+                {title: "Bottega", category: "Education"}, 
+                {title: "Ivanti", category: "Software Development"}
             ]
         };
 
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
     }
     
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
+    }
+
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={"umass.edu"}/>;
@@ -31,15 +41,30 @@ export default class PortfolioContainer extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+        
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
+
+                <div className="filter-buttons">
+                    <button onClick={() => this.handleFilter("Education")} className="filter-button">
+                        Education
+                    </button>
+                    <button onClick={() => this.handleFilter("Civil Engineering")} className="filter-button">
+                        Civil Engineering
+                    </button>
+                    <button onClick={() => this.handleFilter("Software Development")} className="filter-button">
+                        Software Development
+                    </button>
+                </div>
+
                 <div className="portfolio-items">
                     {this.portfolioItems()}
                 </div>
-
-                <hr/>
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
+                
 
             </div>
         )
