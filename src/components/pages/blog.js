04dfb049680e18a21b25466ac13a860f09485758
hdensten <hannah.densten@gmail.com>
2019-10-15 22:29:21 -0600

    Started blog api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://hannahdensten.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
-    return (
-      <div>
-        <h2>Blog</h2>
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
 
-        <div>
-          <Link to="/about-me">Read more about myself</Link>
-        </div>
-      </div>
-    );
+    return <div>{blogRecords}</div>;
   }
 }
 
