018932fc64929ea87a111256be5783f5667aaa35
hdensten <hannah.densten@gmail.com>
2019-10-17 09:53:58 -0600

    Built infinite scroll

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -18,6 +18,9 @@ export default class BlogDetail extends Component {
       )
       .then(response => {
         console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
       })
       .catch(error => {
         console.log("getBlogItem error", error);
@@ -29,10 +32,24 @@ export default class BlogDetail extends Component {
   }
 
   render() {
-    console.log("currentId", this.state.currentId);
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
-      <div>
-        <h1>Blog detail</h1>
+      <div className="blog-container">
+        <div className="content-container">
+          <h1>{title}</h1>
+
+          <div className="featured-image-wrapper">
+            <img src={featured_image_url} />
+          </div>
+
+          <div className="content">{content}</div>
+        </div>
       </div>
     );
   }
