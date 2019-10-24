7f3582f7bd671cfd62333c1f30483c7adee7ab8d
hdensten <hannah.densten@gmail.com>
2019-10-23 20:18:48 -0600

    connected to blog update api endpoint

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -16,6 +16,25 @@ export default class BlogDetail extends Component {
     };
 
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false
+    });
+  }
+
+  handleFeaturedImageDelete() {
+    this.setState({
+      blogItem: {
+        featured_image_url: ""
+      }
+    });
   }
 
   handleEditClick() {
@@ -23,13 +42,12 @@ export default class BlogDetail extends Component {
     this.setState({ editMode: true });
   }
 
-  getBlogItem = () => {
+  getBlogItem() {
     axios
       .get(
         `https://hannahdensten.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
       )
       .then(response => {
-        console.log("response", response);
         this.setState({
           blogItem: response.data.portfolio_blog
         });
@@ -37,7 +55,7 @@ export default class BlogDetail extends Component {
       .catch(error => {
         console.log("getBlogItem error", error);
       });
-  };
+  }
 
   componentDidMount() {
     this.getBlogItem();
@@ -54,7 +72,12 @@ export default class BlogDetail extends Component {
     const contentManager = () => {
       if (this.state.editMode) {
         return (
-          <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />
+          <BlogForm
+            handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+            editMode={this.state.editMode}
+            blog={this.state.blogItem}
+          />
         );
       } else {
         return (
