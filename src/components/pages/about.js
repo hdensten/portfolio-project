2a79b45bb0b20d985ef2fc9fbfc2b58bcd3c02fb
hdensten <hannah.densten@gmail.com>
2019-10-23 21:27:41 -0600

    updates to pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,82 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
 
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem quae nemo
+        natus hic suscipit ea enim, nihil adipisci ad tempora autem sapiente
+        iusto accusantium, dolorem consequatur obcaecati recusandae assumenda
+        illum. Repudiandae quidem tenetur vero vitae, alias, tempore aperiam
+        dolorum tempora blanditiis reiciendis molestiae exercitationem. Veniam
+        vel suscipit corporis deleniti aliquid? Sed placeat sequi ea at quas
+        atque explicabo a soluta! Voluptatem nostrum eos unde odit aspernatur
+        asperiores mollitia aperiam. Sit sed maiores nulla, esse soluta
+        explicabo voluptatibus omnis saepe a at ea officiis amet quidem
+        excepturi assumenda laboriosam est perferendis. Laudantium minus
+        necessitatibus quam maiores quis repudiandae dicta molestiae quos
+        tempore iure, exercitationem, optio dolorem quod obcaecati mollitia
+        sequi nostrum amet ea. In vitae eius minima numquam cupiditate beatae
+        itaque. Aliquid dignissimos pariatur placeat est harum ullam quam, iusto
+        eligendi adipisci laborum. Incidunt velit similique quasi assumenda et
+        illum fuga magni natus, suscipit esse numquam eum id expedita quidem
+        eveniet. Dolores quae repudiandae quia earum eius similique, tempora
+        placeat corrupti dolor, ad ipsum minima consectetur, mollitia sint
+        repellat quis totam architecto error. Magni necessitatibus quasi vel
+        possimus officia quidem. Quis. Est quidem quaerat quod dolorum ipsum
+        aspernatur excepturi id similique, aperiam ipsa reiciendis inventore
+        blanditiis, nemo maiores nostrum tempora error? Delectus quae culpa
+        pariatur. Nemo saepe enim voluptates reprehenderit atque. Ipsam modi
+        aspernatur ut id ullam ab rem voluptatum molestias, vitae dolores
+        laboriosam dolorem dolore. Dolor, laudantium sequi? Quia odit dolores,
+        totam itaque voluptates consectetur vero sint ducimus sunt mollitia?
+        Minus, fuga, praesentium quae aliquid est, alias aliquam expedita sunt
+        repellendus fugit at dolorum natus. Provident dicta velit impedit error
+        necessitatibus optio cumque deleniti fuga, quasi exercitationem,
+        eligendi veniam beatae! Natus facilis, vero magnam quibusdam praesentium
+        labore! Repudiandae, vero fugiat porro veritatis culpa, quibusdam, hic
+        facere eius odit reprehenderit earum dignissimos fuga perspiciatis
+        voluptatibus. Necessitatibus magnam tempora officiis voluptates aut.
+        Vero, reprehenderit! Magni mollitia dolorum dignissimos ex omnis.
+        Repellendus a eius vel tenetur quibusdam voluptatem et non quisquam
+        dolorem obcaecati ipsam est, aliquid quasi quo perspiciatis commodi,
+        dolorum neque! Architecto. Deserunt totam, amet eius beatae modi nostrum
+        ratione, dignissimos perspiciatis molestiae sequi fugiat blanditiis sint
+        eos, tempore sed earum doloribus. Nesciunt nisi molestiae iure obcaecati
+        sequi delectus aspernatur excepturi voluptatibus. Expedita, culpa totam
+        error, accusamus fuga adipisci obcaecati unde esse magnam explicabo
+        doloribus praesentium velit exercitationem omnis mollitia atque,
+        corrupti consequatur necessitatibus iusto repellat? Voluptatibus velit
+        in sed dolor laborum? Pariatur odio distinctio, eius, impedit sed
+        expedita harum dolor sapiente nisi iusto accusantium corporis ut nemo
+        veniam? Placeat modi, error cumque asperiores veritatis cum praesentium
+        illum deleniti labore, maiores quae. Earum quae quis nam inventore
+        libero nemo modi, quasi impedit enim cupiditate sed dignissimos sequi
+        asperiores maiores! Eos officia dolore expedita error maxime temporibus
+        voluptatibus quibusdam odit quas? Sed, minus. Magni aut minima
+        explicabo, tempora quas dolor praesentium modi illum vel minus quibusdam
+        quasi consequuntur est nam odio eius porro ipsum repellat aliquid
+        exercitationem! Ratione vero accusantium dolores itaque enim. A sed
+        tempora ut soluta repellat accusantium, culpa unde dignissimos, in
+        minima esse quo earum excepturi eveniet facilis, nobis officia nisi qui?
+        Odit iusto asperiores cupiditate quibusdam aliquid quos corrupti? Quia
+        ipsum est voluptate! Beatae facere ipsum quibusdam inventore fugiat quae
+        fuga. Nihil tempora quia possimus totam, nisi quasi est dicta provident,
+        perferendis, illum minima quo aperiam facilis distinctio similique. Nam
+        quo recusandae veniam, iusto eos consequatur asperiores voluptates
+        commodi unde corrupti dolorem deleniti modi necessitatibus quos aperiam
+        nobis vitae, eveniet minima voluptas, itaque facilis voluptatem
+        sapiente.
+      </div>
+    </div>
+  );
 }
